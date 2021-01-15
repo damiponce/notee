b3efa1449ba86c8e655d9d18c3e9aecf30ba2e3e
Damián Ponce <dami.ponce8@gmail.com>
2021-01-15 03:32:08 -0300

    Commit after .gitignore fix

--- assets/SFSymbols.js
+++
@@ -1,8 +0,0 @@
-import {createIconSet} from 'react-native-vector-icons';
-import SFSymbolsConfig from './fonts/SFSymbols.json';
-
-export default createIconSet(
-  SFSymbolsConfig,
-  'SF Symbols React Regular',
-  './fonts/SFSymbolsReact.ttf',
-);
